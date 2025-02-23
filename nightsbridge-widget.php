c0ca51396a041b1ef8a83236181a187492451fe5
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2025-02-23 21:56:42 +0200

    Update license

--- nightsbridge-widget.php
+++ nightsbridge-widget.php
@@ -7,7 +7,7 @@
  * Author: Melanie Shepherd
  * Author URI: https://wpdevs.co.za
  * Text Domain:  nightsbridge
- * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ * License URI: https://mit-license.org/
  */
 
 defined('ABSPATH') || exit;
